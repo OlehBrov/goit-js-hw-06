86946b1c1d164bc4da38061c1d5000214afd8237
OlehBrov <o.dudnyk85@gmail.com>
2022-12-19 17:15:46 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsRef = document.querySelector('#ingredients');
+console.log('ingredientsRef:', ingredientsRef)
+
+const ingredientsItem = ingredients.map((item) => {
+  const listItem = document.createElement('li');
+  listItem.textContent = item;
+  listItem.classList = "item"
+  ingredientsRef.append(listItem);
+
+});
