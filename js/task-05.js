9593e4d32e79a41fdb3f74e0d297ac82bc7c5614
OlehBrov <o.dudnyk85@gmail.com>
2022-12-24 17:29:07 +0200

    task-06

--- js/task-05.js
+++ js/task-05.js
@@ -5,6 +5,7 @@ const nameOutputRef = document.querySelector("#name-output");
 
 nameInputRef.addEventListener("input", (event) => {
     if (event.currentTarget.value !== '') {
+        console.dir(event.currentTarget.elements)
         return nameOutputRef.textContent = event.currentTarget.value;
     }
    
