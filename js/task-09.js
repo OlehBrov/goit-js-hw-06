ddc98b620f6beac4ee60b032cf16586496e6d74b
OlehBrov <o.dudnyk85@gmail.com>
2022-12-25 18:08:19 +0200

    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnRef = document.querySelector('.change-color');
+const colorCodeRef = document.querySelector('.color');
+const bodyRef = document.querySelector('body');
+
+btnRef.addEventListener('click', changeColor);
+
+function changeColor() {
+bodyRef.style.backgroundColor = getRandomHexColor();
+colorCodeRef.textContent = getRandomHexColor();
+};
