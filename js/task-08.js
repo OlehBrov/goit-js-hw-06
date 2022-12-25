10e7a95c7cd15cae673e10ac0cc22064611cedd9
OlehBrov <o.dudnyk85@gmail.com>
2022-12-25 16:08:38 +0200

    task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,15 @@
+const loginFormRef = document.querySelector('.login-form')
 
+loginFormRef.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const {
+        elements: { email, password }
+      } = event.currentTarget;
+    
+      if (email.value === "" || password.value === "") {
+        alert ("Please fill in all the fields!");
+      }
+    
+      console.log(`Email: ${email.value}, Password: ${password.value}`);
+      event.currentTarget.reset();
+});
